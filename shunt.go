7baad3f191bbf9ecac1620fbf40fbd478611eed9
Eoghan-oconnor <G00330972@gmit.ie>
2018-04-06 00:54:56 +0100

    Adding intopost functionality

--- shunt.go
+++ shunt.go
@@ -1,17 +1,42 @@
 package main
 
-import (
-	"fmt"
-)
-
+//This function is used to change infix notation to postfix notation
 func intopost(infix string) string {
 	specials := map[rune]int{'*': 10, '.': 9, '|': 8}
 	pofix := []rune{}
 	s := []rune{}
 
+	for _, r := range infix {
+		switch {
+		case r == '(':
+			s = append(s, r)
+		case r == ')':
+			for s[len(s)-1] != '(' {
+				pofix = append(pofix, s[len(s)-1])
+				s = s[:len(s)-1]
+			}
+			s = s[:len(s)-1]
+		case specials[r] > 0:
+			for len(s) > 0 && specials[r] <= specials[s[len(s)-1]] {
+
+				pofix = append(pofix, s[len(s)-1])
+				s = s[:len(s)-1]
+			}
+
+			s = append(s, r)
+
+		default:
+			pofix = append(pofix, r)
+		}
+	}
+	for len(s) > 0 {
+		pofix = append(pofix, s[len(s)-1])
+		s = s[:len(s)-1]
+	}
+	return string(pofix)
 }
 
-func main() {
+/*func main() {
 
 	//Answear ab.c*
 	fmt.Println("infix:	  ", "a.b.c*")
@@ -28,4 +53,4 @@ func main() {
 	//Answer abb.*.c
 	fmt.Println("Infix:  ", "a.(b.b)*.c")
 	fmt.Println("Postfix: ", intopost("a.(b.b)*.c"))
-}
+}*/
