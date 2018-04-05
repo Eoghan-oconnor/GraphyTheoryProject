60e701a816c8f5065e2794961d8850cba0dd6985
Eoghan-oconnor <G00330972@gmit.ie>
2018-04-06 00:57:12 +0100

    Adding Thompsons Algorithm

---
+++ nfa.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+)
+
+type state struct {
+	symbol rune
+	edge1  *state
+	edge2  *state
+}
+
+type nfa struct {
+	initial *state
+	accept  *state
+}
+
+func poregtonfa(pofix string) *nfa {
+	nfaStack := []*nfa{}
+
+	for _, r := range pofix {
+		switch r {
+		case '.':
+			frag2 := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+			frag1 := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+
+			frag1.accept.edge1 = frag2.initial
+
+			nfaStack = append(nfaStack, &nfa{initial: frag1.initial, accept: frag2.accept})
+		case '|':
+			frag2 := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+			frag1 := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+
+			accept := state{}
+			initial := state{edge1: frag1.initial, edge2: frag2.accept}
+			frag1.accept.edge1 = &accept
+			frag2.accept.edge1 = &accept
+
+			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
+		case '*':
+			frag := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+
+			accept := state{}
+			initial := state{edge1: frag.initial, edge2: &accept}
+			frag.accept.edge1 = frag.initial
+			frag.accept.edge2 = &accept
+
+			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
+		default:
+			accept := state{}
+			initial := state{symbol: r, edge1: &accept}
+			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
+		} //end switch
+	} //end for
+
+	return nfaStack[0]
+}
+
+func main() {
+	nfa := poregtonfa("ab.c*|")
+	fmt.Println(nfa)
+}
