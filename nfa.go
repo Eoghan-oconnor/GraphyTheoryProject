a3566558858bc16b063a79c3e4abc1f7702dc142
Eoghan-oconnor <G00330972@gmit.ie>
2018-04-06 01:16:31 +0100

    adding more functionality to nfa.go

--- nfa.go
+++ nfa.go
@@ -51,6 +51,24 @@ func poregtonfa(pofix string) *nfa {
 			frag.accept.edge2 = &accept
 
 			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
+
+		case '?':
+			frag := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+
+			initial := state{edge1: frag.initial, edge2: frag.accept}
+
+			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: frag.accept})
+		case '+':
+			frag := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+
+			accept := state{}
+			initial := state{edge1: frag.initial, edge2: &accept}
+
+			frag.accept.edge1 = &initial
+
+			nfaStack = append(nfaStack, &nfa{initial: frag.initial, accept: &accept})
 		default:
 			accept := state{}
 			initial := state{symbol: r, edge1: &accept}
@@ -61,7 +79,52 @@ func poregtonfa(pofix string) *nfa {
 	return nfaStack[0]
 }
 
+//Add state function
+//This function returns an array of pointers back to the state Struct
+func addState(l []*state, s *state, a *state) []*state {
+	l = append(l, s)
+
+	if s != a && s.symbol == 0 {
+		l = addState(l, s.edge1, a)
+		if s.edge2 != nil {
+			l = addState(l, s.edge2, a)
+		}
+	}
+	return l
+} //End addState function
+
+func pomatch(po string, s string) bool {
+	ismatch := false
+
+	ponfa := poregtonfa(po)
+
+	current := []*state{}
+	next := []*state{}
+
+	current = addState(current[:], ponfa.initial, ponfa.accept)
+
+	for _, r := range s {
+		for _, c := range current {
+			if c.symbol == r {
+				next = addState(next[:], c.edge1, ponfa.accept)
+			} //End if
+		} //End for
+		current, next = next, []*state{}
+	} //End for
+
+	for _, c := range current {
+		if c == ponfa.accept {
+			ismatch = true
+			break
+		} //End if
+
+	} //End range current for
+
+	//return true/false result
+	return ismatch
+}
+
 func main() {
-	nfa := poregtonfa("ab.c*|")
+	nfa := poregtonfa("ab.c*|", "ccc")
 	fmt.Println(nfa)
 }
